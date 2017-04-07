69933f847a8d1ef6b88b1412fe8978aa36505cef
James Jeffryes <jamesgjeffryes@gmail.com>
2017-04-07 15:48:36 -0500

    from_mine taxonomy updating

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup
 
 setup(name='taxaspec',
       version='0.1',
-      description='Acquired & filter mass spectral libraries based on sample '
+      description='Acquire & filter mass spectral libraries based on sample '
                   'taxonomy',
       url='http://github.com/JamesJeffryes/taxaspec',
       author='James Jeffryes',
       author_email='jamesgjeffryes@gmail.com',
       license='MIT',
+      extras_require={'update':  ["pymongo"]},
       )
--- taxaspec/update.py
+++ taxaspec/update.py
@@ -1,3 +1,50 @@
 """
 Methods for updating taxonomic models
 """
+import pymongo
+import pickle
+import sys
+from collections import defaultdict
+
+
+def from_mine(db_name):
+    """Import metabolic models from a MINE database and overwrite taxonomy
+
+    :param db_name: Name of MINE database
+    :type db_name: str
+    :return:
+    :rtype:
+    """
+    print("Updating taxonomy with models from %s database" % db_name)
+    client = pymongo.MongoClient()
+    db = client[db_name]
+    compounds = {}
+
+    def get_comp_data(value, key='DB_links.KEGG'):
+        if value not in compounds:
+            res = db.compounds.find_one({key: value},
+                                        {"Names": 1, "Inchikey": 1, "_id": 0})
+            if res:
+                compounds[value] = res
+            else:
+                return None
+        return compounds[value]["Names"], compounds[value]["Inchikey"]
+
+    names = defaultdict(set)
+    inchikeys = defaultdict(set)
+    for model in db.models.find({}, {'Compounds': 1}):
+        m_id = model['_id']
+        for _id in model['Compounds']:
+            data = get_comp_data(_id)
+            if data:
+                names[m_id].update(data[0])
+                inchikeys[m_id].add(data[1])
+    with open('model_names.pkl', 'wb') as outfile:
+        pickle.dump(names, outfile)
+    with open('model_inchikeys.pkl', 'wb') as outfile:
+        pickle.dump(inchikeys, outfile)
+
+
+if __name__ == "__main__":
+    from_MINE(sys.argv[1])
+    print("Taxonomy updated")
